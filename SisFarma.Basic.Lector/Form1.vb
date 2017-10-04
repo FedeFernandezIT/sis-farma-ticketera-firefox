4888d91c3277ddea5189cabb35c287152cbb1dce
Federico Fernandez <fede.fernandez.it@gmail.com>
2017-10-04 06:05:24 -0300

    reducir modelo

--- SisFarma.Basic.Lector/Form1.vb
+++ SisFarma.Basic.Lector/Form1.vb
@@ -1,3 +1,27 @@
-﻿Public Class Form1
+﻿Public Class Form1    
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            Lector.LeerFicherosConfiguracion
+        Catch ex As Exception
+            MessageBox.Show("Un error ha ocurrido.")
+        End Try        
+    End Sub
+
+    Private Sub Form1_Disposed(sender As Object, e As EventArgs) Handles Me.Disposed
+        Lector.ManageKeyLogger(False)
+    End Sub
+
+    Private Sub Form1_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+        Lector.ManageKeyLogger(False)
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Try
+            Lector.ManageKeyLogger(False)
+            Lector.ManageKeyLogger(True)
+        Catch ex As Exception
+            MessageBox.Show("Un error ha ocurrido. En el Timer")
+        End Try        
+    End Sub
 End Class
